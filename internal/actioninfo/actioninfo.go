138cc1a1f6819571235535469ef5f371b719e004
GregoryP96 <grishapopkov80@gmail.com>
2025-02-28 18:59:40 +0500

    functional package actioninfo

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,11 +1,21 @@
 package actioninfo
 
-import (
-	...
-)
+import "fmt"
 
 // создайте интерфейс DataParser
-...
+type DataParser interface {
+	Parse(string) error
+	ActionInfo() string
+}
 
 // создайте функцию Info()
-...
+func Info(dataset []string, dp DataParser) {
+	for _, val := range dataset {
+		err := dp.Parse(val)
+		if err != nil {
+			fmt.Println("Error:", err)
+			continue
+		}
+		dp.ActionInfo()
+	}
+}
